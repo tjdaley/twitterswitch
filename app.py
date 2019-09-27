7ac5bfb5185ebeb74c16df2ddb65630b5ed7717e
Tom Daley <tjd@powerdaley.com>
2019-09-27 00:45:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,10 @@ def connect_twitter(config):
   Returns:
     Reference to connected Twitter service.
   """
-  api = twitter.Api(consumer_key=config["consumer_key"],
-                    consumer_secret=config["consumer_secret"],
-                    access_token_key=config["access_token_key"],
-                    access_token_secret=config["access_token_secret"],
+  api = twitter.Api(consumer_key=config["CONSUMER_KEY"],
+                    consumer_secret=config["CONSUMER_SECRET"],
+                    access_token_key=config["ACCESS_TOKEN_KEY"],
+                    access_token_secret=config["ACCESS_TOKEN_SECRET"],
                     sleep_on_rate_limit=True)
 
   verified_credentials = api.VerifyCredentials()
