9ae5ebcf3e8b129ba2c18ef8a382be1861fb7708
Tom Daley <tjd@powerdaley.com>
2019-09-26 21:57:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ import RPi.GPIO as GPIO
 import time
 
 def main(args):
+  print("Starting . . .")
   pins = [11, 13, 15, 16]
   
   # Reference channels by physical pin number
+  print("Reference pins by physical board position.")
   GPIO.setmode(GPIO.BOARD)
   
   for pin in pins:
+    print(f"Setting pin {pin} to output mode")
     GPIO.setup(pin, GPIO.OUT)
 
   for pin in pins:
+    print(f"Raising pin {pin}")
     GPIO.output(pin, GPIO.HIGH)
     
+  print("Sleeping for 10 seconds")
   time.sleep(10)
   
   for pin in pins:
+    print(f"Droping pin {pin}")
     GPIO.output(pin, GPIO.LOW)
-    
+  
+  print("Cleaning up")
   GPIO.cleanup()
     
   
