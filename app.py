15c3bc916135f115f5b860918b543a65bb705854
Tom Daley <tjd@powerdaley.com>
2019-09-27 01:10:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ LATITUDE = "33.172584"
 LONGITUDE = "-96.665638"
 RADIUS = "1mi"  # mi=miles; km=kilometers
 HASHTAG = "%23avianaart"
-QUERY = f'q={HASHTAG}&geocode=({LATITUDE},{LONGITUDE},{RADIUS})&src=typd'
-#QUERY = 'q=%23avianaart%20near%3A"3011%20chukar%20dr%2C%20mckinney%2C%20tx"%20within%3A15mi&src=typd'
+QUERY = f'q={HASHTAG}&geocode=({LATITUDE},{LONGITUDE},{RADIUS})&count=1'
+#QUERY = 'q=%23avianaart%20near%3A"3011%20chukar%20dr%2C%20mckinney%2C%20tx"%20within%3A15mi'
 PINS = [11, 13, 15, 16]
 
 def setup_gpio(pins: list):
